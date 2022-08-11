12ac82cfeb78a19d0646102779245327fb67215c
Vlasam <vlad.samorukov.99@mail.ru>
2022-08-11 13:56:10 +0300

    Найти максимальное из трех чисел

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿int a = new Random().Next(1,10);
+Console.WriteLine("Первое число: "+a);
+int b = new Random().Next(1,10);
+Console.WriteLine("Второе число: "+b);
+int c = new Random().Next(1,10);
+Console.WriteLine("Третье число: "+c);
+if (a > b )
+{
+    Console.WriteLine("Самое большое число из a и b: "+a);
+}
+else 
+{
+    Console.WriteLine("Самое большое число из a и b: "+b);
+}
+if (a > c)
+{
+    Console.WriteLine("Самое большое число из a и c: "+a);
+}
+else
+{
+    Console.WriteLine("Самое большое число из a и b: "+c);
+}
+if (b > c)
+{
+    Console.WriteLine("Самое большое число из a и b: "+b);
+}
+else
+{
+    Console.WriteLine("Самое большое число из a и b: "+c);
+}
