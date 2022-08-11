e7a195f0bd414e9805670f2cc10fe06be26c4a23
Vlasam <vlad.samorukov.99@mail.ru>
2022-08-11 14:43:19 +0300

    Выяснить является ли число чётным

--- Program.cs
+++ Program.cs
@@ -1,30 +1,10 @@
-﻿int a = new Random().Next(1,10);
+﻿double a = new Random().Next(1,1000);
 Console.WriteLine("Первое число: "+a);
-int b = new Random().Next(1,10);
-Console.WriteLine("Второе число: "+b);
-int c = new Random().Next(1,10);
-Console.WriteLine("Третье число: "+c);
-if (a > b )
+if (a % 2 ==0)
 {
-    Console.WriteLine("Самое большое число из a и b: "+a);
-}
-else 
-{
-    Console.WriteLine("Самое большое число из a и b: "+b);
-}
-if (a > c)
-{
-    Console.WriteLine("Самое большое число из a и c: "+a);
+    Console.WriteLine("Число четное");
 }
 else
 {
-    Console.WriteLine("Самое большое число из a и b: "+c);
-}
-if (b > c)
-{
-    Console.WriteLine("Самое большое число из a и b: "+b);
-}
-else
-{
-    Console.WriteLine("Самое большое число из a и b: "+c);
+    Console.WriteLine("Число нечётное");
 }
